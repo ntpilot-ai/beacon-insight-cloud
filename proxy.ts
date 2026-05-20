6dbaed4fb23bd9d8eaf7e224a331b225a542bb94
Nik Tuson <niktuson@outlook.com@>
2026-05-20 16:51:58 +0100

    Proxy Fix

--- proxy.ts
+++ proxy.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import type { CookieOptions } from "@supabase/ssr";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const response = NextResponse.next();
 
   const supabase = createServerClient(
